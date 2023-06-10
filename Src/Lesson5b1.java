594a4f24986abd21cdca2d9c2703f57017683c06
KaterynaDvernytska <dvernitskayae@gmail.com>
2023-06-11 02:54:53 +0300

    first

---
+++ Src/Lesson5b1.java
@@ -0,0 +1,11 @@
+public class Lesson5b1 {
+    public static void main(String[] args) {
+        int count = 0, value = 0;
+        while (count < 100) {
+            String s = String.valueOf(++value);
+            if ((s.contains("4") || s.contains("9"))) continue;
+            System.out.println(value);
+            count++;
+        }
+    }
+}
